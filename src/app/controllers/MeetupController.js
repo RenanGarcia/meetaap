aaf34cd4193d60de83407bd50beac38cde8e08d7
Renan Nobre Garcia <renn.nobregarcia@gmail.com>
2019-09-25 23:25:32 -0300

    Store request validation

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,9 +1,22 @@
+import * as Yup from 'yup';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
     try {
-      const meetup = await Meetup.create(req.body);
+      const schema = Yup.object().shape({
+        title: Yup.string().required(),
+        description: Yup.string().required(),
+        location: Yup.string().required(),
+        date: Yup.date().required(),
+        banner_id: Yup.number().required(),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation fails' });
+      }
+
+      const meetup = await Meetup.create({ ...req.body, user_id: req.userId });
 
       return res.json(meetup);
     } catch (err) {
